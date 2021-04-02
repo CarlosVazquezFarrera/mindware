833eb5629067461a0332a4a8313b949e5a581bb5
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2021-04-02 16:46:29 -0600

    Implementación de rutas de administrador y protección de rutas

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
-
-Route::get('/dashboardAdmin', 'App\Http\Controllers\DashboardController@index')->name('dashboardAdmin');
-
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboardAdmin', function () {
 //     return view('dashboard.index');
 // })->name('dashboardAdmin');
