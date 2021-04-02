6ce0472e8ab9012b609af320ee4f6bd7a5f2edac
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2021-04-02 16:25:06 -0600

    Extender el AdminLite

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,14 @@
-Vista del dashboard
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('js')
+@stop
